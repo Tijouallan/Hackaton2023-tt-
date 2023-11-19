d2922b032ae963337b111df8804a2944dfdf443d
tijou allan <tijouallan@gmail.com>
2023-11-19 05:54:16 +0100

    page web et leaflet

---
+++ vue/accueil.view.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Da&display=swap" rel="stylesheet">
+
+    <title>√tt</title>
+</head>
+
+<body>
+    <?php require_once('header.php'); ?>
+
+    <section id="presentation_des_modeles">
+        <div class="bulle_titre btn-15">
+            <h2>Le √tt</h2>
+        </div>
+
+
+        <p>
+            Découvrez le futur de la mobilité cycliste avec le √tt, bien plus qu'un simple vélo à assistance électrique.<br> Notre innovation réside dans sa capacité à fusionner, littéralement. Imaginez deux vélos se fondant en un seul, une symbiose de force et d'efficacité.<br> Avec le √tt, la fusion des deux roues arrière transforme votre expérience de cyclisme en une performance harmonieuse, où chaque pédalée unitaire représente l'alliance de deux énergies.
+        </p>
+        <p>
+            La modularité est au cœur de notre conception. Notre √tt s'adapte, se module, et se configure selon vos besoins. Avec sa structure adaptable, ce vélo s'ajuste à différentes tailles, offrant confort et praticité à chaque utilisateur.
+        </p>
+        <p>
+            Bienvenue dans une nouvelle ère du cyclisme, où la fusion devient votre force, la modularité votre liberté et l'adaptabilité votre norme. Découvrez le √tt : Redéfinissez votre trajet, fusionnez vos forces, explorez l'innovation.
+        </p>
+        <div><img id="vtt1" src="../src/images/velo_dalle.png" alt="" s></div>
+        </div>
+
+
+
+    </section>
+    <section>
+        <div class="bulle_titre btn-15">
+            <a href="carte.html">
+                <h2>Acceder à l'application</h2>
+            </a>
+        </div>
+    </section>
+    <section id="technologie_et_innovation">
+        <div class="bulle_titre btn-15">
+            <h2>Technologie et innovation</h2>
+        </div>
+        <img src="" alt="">
+        <p>
+            Notre innovation repose sur une technologie de fusion révolutionnaire, redéfinissant la façon dont les vélos sont assemblés. Chaque composant du VT.T a été conçu pour permettre une fusion ingénieuse, où la réunion des éléments crée une synergie inégalée.
+        </p>
+        <p>
+            La technologie de fusion employée dans la conception du VT.T offre une solidité accrue et une symbiose parfaite entre les parties assemblées. Cela permet non seulement une transformation fluide du vélo en un modèle fusionné, mais garantit également une expérience de conduite sans compromis, quelle que soit la configuration du vélo.
+        </p>
+    </section>
+
+    <section id="experience_utilisateur_et_temoignages">
+        <div class="bulle_titre btn-15">
+            <h2>Expérience utilisateur et témoignages :</h2>
+        </div>
+        <p> Découvrez ce que les cyclistes disent à propos du VT.T, partagez leurs expériences et commentaires positifs !
+        </p>
+        <p> <u>Témoignages :</u></p>
+
+        <div class="card">
+
+            <p> <strong>"Une Révolution du Cyclisme !" - Metz à Vélo</strong> </p>
+            <p> L'association "Metz à Vélo" approuve pleinement le VT.T, offrant une expérience de cyclisme inégalée. Une dizaine de membres ont déjà adopté cette nouvelle approche du vélo et l'ont approuvée avec enthousiasme.
+                Partagez votre expérience :
+                Vous avez essayé le VT.T et souhaitez partager votre expérience ? Rejoignez la conversation ! Partagez vos témoignages et votre expérience avec le VT.T. Votre voix compte, contribuez à façonner l'avenir du cyclisme.
+            </p>
+            <img src="../src/images/PXL_20231118_154831774.jpg" alt="">
+        </div>
+
+    </section>
+
+    <section id="quisommesnous">
+        <h2>Qui sommes-nous</h2>
+        <p>
+            Nous sommes une équipe dynamique d'étudiants passionnés, réunis par une vision commune dans le cadre d'un défi intense de créativité : un hackathon de 24 heures organisé à l'IUT de Metz, Université de Lorraine. Unis par notre diversité de disciplines - Gestion des Entreprises et des Administrations (GEA), Informatique, Techniques de Commercialisation (Tech de Co), Mesures Physiques et Génie Mécanique et Productique (GMP) - nous avons combiné nos expertises variées pour donner vie au √tt.
+        </p>
+        <p>
+            Dans un délai serré, stimulés par la pression du temps et portés par notre passion collective pour l'innovation, nous avons uni nos forces pour repousser les limites du possible dans le monde du cyclisme. Animés par la volonté de créer un vélo qui transcende les normes conventionnelles, nous avons développé le √tt, un vélo à assistance électrique révolutionnaire, fusionnable et adaptable, conçu pour redéfinir la mobilité cycliste.
+        </p>
+        <p>
+            Notre diversité est notre atout, notre collaboration notre moteur. En 24 heures intenses, nous avons incarné la fusion des idées, des compétences et des ambitions pour façonner un avenir où la mobilité devient synonyme d'adaptabilité, d'efficacité et de durabilité. Bienvenue dans l'univers du √tt, fruit de notre passion et de notre détermination à innover pour vous offrir une expérience de cyclisme inégalée.
+        </p>
+    </section>
+</body>
+
+</html>
---
+++ vue/header.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<header>
+    <section id="header" class="header">
+        <img src="..\src\images\propositionbV4.svg" alt="" width="80%" />
+        <span></span>
+        <!-- <p>Bienvenue, <?php echo $nom;
+                            echo ' ';
+                            echo $prenom ?> </p> -->
+        <!--         <nav>
+            <ul class="menu">
+                <li><a href="#presentation_des_modeles">Présentation des modèles</a></li>
+
+                <li><a href="#technologie_et_innovation">Technologie et innovation</a></li>
+                <li><a href="#experience_utilisateur_et_temoignages">Expérience utilisateur et témoignages</a></li>
+                <li><a href="#blog_ou_actualites">Blog ou actualités</a></li>
+                <li><a href="#quisommesnous">Qui sommes nous</a></li>
+                <li><a href="#service_client_et_faq">Service client et FAQ</a></li>
+                <li><a href="#reseaux_sociaux_et_communaute">Réseaux sociaux et communauté</a></li>
+            </ul>
+        </nav> -->
+
+
+    </section>
+</header>
